f5e21c88328f52bb2fdf7aac4a888dc5067417ef
wangbo <781278657@qq.com>
2021-11-30 16:32:28 +0800

    add：封装 Storage 类

---
+++ src/core/Helper/storage/README.md
@@ -0,0 +1,6 @@
+-[x] set：可传入普通值，对象及数组
+-[x] get：不传参，获取所有 key 对应的值，包装成对象
+-[x] delete：可传数组，删一串
+-[x] clear：删除所有
+-[x] keys：返回所有 key，返回 array
+-[x] getAll：获取所有 key 对应的值，包装成对象
---
+++ src/core/Helper/storage/StorageTest.ts
@@ -0,0 +1,60 @@
+type StorageValue = string | number | Record<string, any> | Array<any>
+type GetStorageValue = string | Record<string, string>
+
+class StorageTest {
+    private readonly _storage: Storage
+
+    constructor(storage: Storage) {
+        this._storage = storage
+    }
+
+    get instance(): Storage {
+        return this._storage
+    }
+
+    set(key: string, value: StorageValue): void {
+        this._storage.setItem(key, JSON.stringify(value))
+    }
+
+    get(key?: string): GetStorageValue {
+        return key ? this._storage.getItem(key) : this.getAll
+    }
+
+    delete(keys: string | Array<string>): void {
+        Array.isArray(keys) ? keys.forEach(key => this.remove(key)) : this.remove(keys)
+    }
+
+    private remove(key: string): void {
+        this._storage.removeItem(key)
+    }
+
+    clear(): void {
+        this._storage.clear()
+    }
+
+    get keys(): Array<string> {
+        const {length, ...other} = this._storage
+        return Object.keys(other)
+    }
+
+    get getAll(): Record<string, string> {
+        const {length, ...other} = this._storage
+        return other
+    }
+}
+
+const storage = new StorageTest(localStorage)
+
+storage.set('info', {name: 'bob', age: 18})
+storage.set('age', 18)
+storage.set('name', 'bob')
+
+console.log(storage);
+console.log(storage.get('name'));
+console.log(storage.instance)
+console.log(storage.keys)
+console.log(storage.getAll)
+storage.delete('info')
+storage.delete(['age'])
+console.log(storage.get());
+storage.clear()
